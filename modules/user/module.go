98024383da8e1f7babd6fd1f3ae920de6ed18025
fzerorubigd <fzerorubigd@gmail.com>
2019-08-15 00:46:53 +0200

    fix the user mod inclusion

--- modules/user/module.go
+++ modules/user/module.go
@@ -8,6 +8,8 @@ import (
 	_ "elbix.dev/engine/modules/user/migrations"
 	// Base models and protobuf/grpc code
 	_ "elbix.dev/engine/modules/user/proto"
+	// Add User impl
+	_ "elbix.dev/engine/modules/user/impl"
 )
 
 // TODO : some sort of dependency injection in module level
