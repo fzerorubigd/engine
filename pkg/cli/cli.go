0ce644620eb116413367f8136bbe04d277d3b010
fzerorubigd <fzerorubigd@gmail.com>
2019-12-22 16:57:00 +0100

    [pkg/cli] fix the issue with the signals and context
    os.Notify sends the signal without waiting for the receiver if we create
    many context this way, some of them might miss the signal, since the sig
    channel is out of capacity

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -7,12 +7,15 @@ import (
 	"syscall"
 )
 
-var sig = make(chan os.Signal, 4)
+var signals = []os.Signal{
+	syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGABRT,
+}
 
 // Context returns a context that is cancelled automatically when a kill signal received
 func Context() context.Context {
+	var sig = make(chan os.Signal, len(signals))
 	ctx, cancel := context.WithCancel(context.Background())
-	signal.Notify(sig, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGABRT)
+	signal.Notify(sig, signals...)
 	go func() {
 		<-sig
 		cancel()
